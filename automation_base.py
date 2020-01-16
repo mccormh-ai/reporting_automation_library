ce6f1ebdbda3d96edd7dc44966db2d871c8c1193
Hayden <mccormh@protonmail.com>
2020-01-16 11:24:26 -0600

    Added Dimension Tabl parsing

--- automation_base.py
+++ automation_base.py
@@ -9,10 +9,12 @@ class Auto_Base():
                  ):
         if not isinstance(directory_dict, dict()):
             raise TypeError()
-        for file_key, file_path
+        for file_key, file_path:
+            print(file_key)
         self.branch_dict = dict()
         self.directory_base = base_dir
         self.directory_paths = {"Base":self.directory_base}
+        self.dimension_tbls = dict()
         self.tag = indentifier
         self.general_source_type
 
@@ -27,7 +29,18 @@ class Auto_Base():
     def dynamic_dimension_tbls(self, source_type=None):
         # source_type in ['raw', 'sql']
 
-    def initialize_branch(branch_name, path_extension=None, file_extension=None, table_name=None):
+    def initialize_dimension_tbl(self, name, data_source="flat file",
+                                path_extension=None, all_in_folder=False, file_extension=None, 
+                                table_name=None
+                                return_type="DataFrame"):
+        dimension_payload = dict()
+        dimension_payload['Name'] = name
+        if
+
+        if return_type is "DataFrame":
+            dimension_tbls.append(dimension_payload)
+
+    def initialize_branch(self, branch_name, path_extension=None, file_extension=None, table_name=None):
         # Call to add new platform or path
         if path_extension is not None:
             # New branch is a flat file file
