46c65f2f7e48183e9620f90abf394a3c65323032
Hayden <mccormh@protonmail.com>
2020-01-16 13:59:22 -0600

    Confused by design philosophy, re-think strategem

--- automation_base.py
+++ automation_base.py
@@ -4,7 +4,7 @@ import os
 class Auto_Base():
     # Accept a dict() if data_source = SQL
     # {}
-    def __init__(base_dir=None,
+    def __init__(self, base_dir=None,
                  identifier=datetime.datetime.today().strftime("%m.%d.%y")
                  ):
         if not isinstance(directory_dict, dict()):
@@ -20,7 +20,8 @@ class Auto_Base():
 
 
 
-    def _automation_internal(self, source_type=None)
+    def _automation_internal(self, source_date=**kwargs):
+
 
     def static_dimension_tbls(self, source_type=None, update_condition):
         # source_type in ['raw', 'sql']
@@ -29,8 +30,9 @@ class Auto_Base():
     def dynamic_dimension_tbls(self, source_type=None):
         # source_type in ['raw', 'sql']
 
-    def initialize_dimension_tbl(self, name, data_source="flat file",
-                                path_extension=None, all_in_folder=False, file_extension=None, 
+    def initialize_dimension_tbl(self, name,
+                                ="flat file",
+                                path_extension=None, all_in_folder=False, file_extension=None,
                                 table_name=None
                                 return_type="DataFrame"):
         dimension_payload = dict()
@@ -40,10 +42,24 @@ class Auto_Base():
         if return_type is "DataFrame":
             dimension_tbls.append(dimension_payload)
 
-    def initialize_branch(self, branch_name, path_extension=None, file_extension=None, table_name=None):
+    def initialize_branch(self, branch_name, origin_path_extension=None,
+        file_extension=None, origin_table_name=None,
+        target_path_extension=None,
+        target_table_name=None):
         # Call to add new platform or path
-        if path_extension is not None:
+        branch_payload = dict()
+
+        if origin_path_extension is not None:
             # New branch is a flat file file
-            os.path.join(self.directory_base, path_extension)
+            branch_payload["origin_path"] = os.path.join(self.directory_base, path_extension)
+        if origin_table_name is not None:
+            branch_payload["origin_tbl"] = str(table_name)
+
+        if target_path_extension is not None:
+            # New branch is a flat file file
+            branch_payload["target_path"] = os.path.join(self.directory_base, path_extension)
+        if target_path_extension is not None:
+            branch_payload["target_table"] = str(table_name)
+
 
     def generate_fact_tbls(self, source_type=None):
---
+++ branch_base.py
@@ -0,0 +1,8 @@
+
+
+class Branch():
+    def __init__(self, source_dataframe, primary_key):
+        self.dataframe = source_dataframe
+        self.pk = primary_key
+
+    def 
