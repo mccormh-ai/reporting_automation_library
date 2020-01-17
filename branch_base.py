7b943ee4daddf7b797d84607802c32d6e964890a
Hayden <mccormh@protonmail.com>
2020-01-17 10:48:27 -0600

    Added datetime operations

--- branch_base.py
+++ branch_base.py
@@ -1,8 +1,16 @@
+import pandas as pd
+from dataframe_lib.datetime_operations import convertDateColumns
 
 
 class Branch():
-    def __init__(self, source_dataframe, primary_key):
+    def __init__(self, source_dataframe, primary_key, date_column=None):
         self.dataframe = source_dataframe
-        self.pk = primary_key
+        self.pk = primary_key,
+        if date_column is not None:
+            self.dataframe[date_column] = pd.to_datetime(self.dataframe[date_column])
+        else:
+            self.dataframe = convertDateColumns(self.dataframe)
 
-    def 
+    def column_manipulation(self, base_dimension_df):
+        # deal with rename_columns
+        # 
